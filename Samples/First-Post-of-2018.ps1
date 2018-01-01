e6a6ca487b0e56f37f08150d674d6c91231f7726
lucdekens <dekens.luc@gmail.com>
2018-01-01 14:06:43 +0100

    Improved code

--- Samples/First-Post-of-2018.ps1
+++ Samples/First-Post-of-2018.ps1
@@ -1,8 +1,7 @@
 ﻿Initialize-VMTNRest
 
-Get-VMTNContent -Start (Get-Date "Jan 1 2018") |
-Sort-Object -Property published |
-Select-Object -First 1 -Property subject,
+Get-VMTNContent -Start (Get-Date "Jan 1 2018") -MaxSamples 1 |
+Select-Object -Property subject,
 @{N='Published';E={([DateTime]$_.published).ToLocalTime()}},
     @{N='Author';E={$_.author.displayName}},
     @{N='Community';E={$_.parentPlace.name}}
