535d82846422a810dfe1da66a20d86c9b0495dcb
lucdekens <dekens.luc@gmail.com>
2018-01-01 13:57:44 +0100

    Add sample - First post of 2018

---
+++ Samples/First-Post-of-2018.ps1
@@ -0,0 +1,8 @@
+﻿Initialize-VMTNRest
+
+Get-VMTNContent -Start (Get-Date "Jan 1 2018") |
+Sort-Object -Property published |
+Select-Object -First 1 -Property subject,
+@{N='Published';E={([DateTime]$_.published).ToLocalTime()}},
+    @{N='Author';E={$_.author.displayName}},
+    @{N='Community';E={$_.parentPlace.name}}
