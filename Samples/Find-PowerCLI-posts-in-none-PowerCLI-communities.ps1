39c08d44c45a5b55651fce0019549fa315ad9aa6
lucdekens <dekens.luc@gmail.com>
2018-01-03 09:39:32 +0100

    PowerCLI posts in none-PowerCLI communities

---
+++ Samples/Find-PowerCLI-posts-in-none-PowerCLI-communities.ps1
@@ -0,0 +1,10 @@
+﻿Initialize-VMTNRest
+
+$pcliCommunities = Get-VMTNCommunity -Community powercli -IncludeGroup | select -ExpandProperty name
+
+Get-VMTNContent -Keyword powercli -Start (Get-Date).AddMonths(-1) |
+where{$pcliCommunities -notcontains $_.parentPlace.name} |
+select subject,status,resolved,
+    @{N='Author';E={$_.author.displayName}},
+    @{N='Location';E={$_.parentPlace.name}},
+    @{N='Published';E={([DateTime]$_.published).ToLocalTime()}}
